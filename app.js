6547f490588646f125545c491f87f148c79651d5
sdepold <sascha@depold.com>
2012-05-10 06:35:35 +0200

    scrum board v2

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const express   = require("express")
 // variables
 
 var sequelize = new Sequelize(config.dbName, config.dbUsername, config.dbPassword, {
-      logging: false,
-      dialect: 'sqlite',
-      storage: 'database.sqlite'
+      logging: false//,
+      // dialect: 'sqlite',
+      // storage: 'database.sqlite'
     })
   , tracker   = new Tracker(sequelize, config.token, {
       projectId: config.projectId
@@ -69,6 +69,17 @@ app.get('/', function(req, res) {
   })
 })
 
+app.get('/scrum-board', function(req, res) {
+  Activity.count().success(function(cnt) {
+    res.render('scrum-board', {
+      title: 'Pivotal TimeTravel',
+      config: config,
+      activityCount: cnt,
+      boardLayout: 'state'
+    })
+  })
+})
+
 app.post('/activities', function(req, res) {
   var username = req.param('username')
 
@@ -92,6 +103,27 @@ app.post('/activities', function(req, res) {
     console.log('no user found with name: ' + username)
     res.send('-')
   }
+})
+
+app.get('/activities', function(req, res) {
+  var updatedAt = req.param('layout') == 'state'
+    ? moment(req.param("sprintStart")).toDate()
+    : moment().subtract('days', req.param('range') || 2).sod().toDate()
+
+  Activity.findAll({
+    where: [ "updatedAt > ?", updatedAt ]
+  }).success(function(activities) {
+    var activitiesPerUser = {}
+
+    activities.forEach(function(activity) {
+      activitiesPerUser[activity.ownedBy] = activitiesPerUser[activity.ownedBy] || []
+      activitiesPerUser[activity.ownedBy].push(activity)
+    })
+
+    res.json(activitiesPerUser)
+  }).error(function(err) {
+    res.send(err)
+  })
 })
 
 // bind app to port
---
+++ public/javascripts/src/layout.scrum-board.js
@@ -0,0 +1,79 @@
+Layout.ScrumBoard = (function() {
+  "use strict"
+
+  var scrumStates = {
+    'Unstarted': 'Unstarted',
+    'Started':   'Started',
+    'Finished':  'Finished',
+    'Delivered': 'Delivered',
+    'Rejected':  'Rejected',
+    'Accepted':  'Accepted'
+  }
+
+  var ScrumBoard = function(selector) {
+    this.element = $(selector)
+    this.table   = $('<table class="well" cellpadding="0" cellspacing="0" border="0">').appendTo(this.element)
+  }
+
+  ScrumBoard.prototype.render = function() {
+    var self = this
+    getActivities.call(this, function(data) {
+      for(var username in data) {
+        if(username !== "null") {
+          renderUserRow.call(self, username, data[username])
+        }
+      }
+
+      renderUserRow.call(self, 'Unassigned', data['null'])
+    })
+
+    renderHeadlines.call(this)
+  }
+
+  /////////////
+  // private //
+  /////////////
+
+  var getActivities = function(callback) {
+    console.log(callback)
+    $.getJSON('/activities', {
+      sprintStart: Application.Index.SprintStart.getValue()
+    }).success(function(data) {
+      callback && callback(data)
+    }).error(function(err) {
+      console.log(err)
+    })
+  }
+
+  var renderHeadlines = function() {
+    var tr = $('<tr>').appendTo(this.table)
+
+    tr.append($('<th>'))
+
+    for(var state in scrumStates) {
+      tr.append($('<th>').text(scrumStates[state]))
+    }
+  }
+
+  var renderUserRow = function(username, activities) {
+    var tr = $('<tr>').appendTo(this.table)
+
+    tr.append($('<td>').text(username))
+
+    for(var state in scrumStates) {
+      tr.append($('<td>').attr('data-state', state.toLowerCase()))
+    }
+
+    activities.forEach(function(activity) {
+      var story = $('<div class="activity ' + activity.storyType + '">')
+        .text(activity.title)
+        .appendTo($('[data-state=' + activity.status.toLowerCase() + ']', tr))
+
+      story.append(
+        $('<div class="label story-type">').text(activity.storyType)
+      )
+    })
+  }
+
+  return ScrumBoard
+})()
